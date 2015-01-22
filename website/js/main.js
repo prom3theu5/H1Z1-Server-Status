3153ae787cf46a52ee4eb5b199fc887093090ef3
prom3theu5 <dave.sekula@gmail.com>
2015-01-22 04:41:33 +0000

    added progress bar

--- website/js/main.js
+++ website/js/main.js
@@ -1,8 +1,10 @@
 var app = angular.module("h1z1", []);
 app.controller("MainController", function($scope, $http) {
-    $http.get('http://prom3theu5api.azurewebsites.net/api/Services/GetServerStatuses').
+    $scope.loading = true;
+	$http.get('http://prom3theu5api.azurewebsites.net/api/Services/GetServerStatuses').
     success(function(data, status, headers, config) {
       $scope.Servers = data;
+	  $scope.loading = false;
     }).
     error(function(data, status, headers, config) {
       console.log(data);
